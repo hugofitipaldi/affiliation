25eeb389aaa6387848f3e461795e1804d50751fc
Hugo Fitipaldi <33068886+hugofitipaldi@users.noreply.github.com>
2022-10-24 10:28:08 +0200

    some error deletions

--- R/auth_aff_dict.R
+++ R/auth_aff_dict.R
@@ -15,7 +15,6 @@
 #' @examples
 #' authors_names <- "John Doe,1 Jane Doe,2,3"
 #' authors_names <- gsub("([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-9][0-9])","\\1,",authors_names)
-#' authors_name <- gsub(",",",",authors_names)
 #' affiliation_dict <- "1 Affiliation ONE, Recife, Brazil\n2 Affiliation TWO, Kent, Ohio, United States\n3 Affiliation THREE, Malmo, Sweden"
 #' auth_aff_dict(authors_names, affiliation_dict)
 
